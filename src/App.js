110d6ed517d9be8256afd03c0516a61c0aca46f5
Qu4k3r <neves0007@gmail.com>
2021-03-01 22:00:42 -0300

    Exercicios 1 & 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      message: '',
+      status: '',
+      loading: true,
+    }
+  }
+
+  getRandomDogPic = async () => {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const dogObj = await response.json();
+
+    this.setState({
+      message: dogObj.message,
+      status: dogObj.status,
+      loading: false,
+    });
+  }
+
+  randomDogImage = () => {
+    this.setState(({ loading: load }) => ({ loading: !load }), () => this.getRandomDogPic());
+  }
+
+  render() {
+    const { message, loading } = this.state;
+    const loadingElement = <span>Loading ...</span>
+    const dogImageElement = <img src={ message } alt="random dog" />
+
+    return (
+      <div className='App-header'>
+        { loading ? loadingElement : dogImageElement }
+        <button onClick={ this.randomDogImage }>Buscar um doguinho</button>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.getRandomDogPic();
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('dogImage', this.state.message);
+  }
+
+  shouldComponentUpdate(_nextProps, nextState) {
+    const { message } = nextState;
+    if (message.includes('terrier')) return false;
+    return true;
+  }
+
 }
 
 export default App;
